8449907a4b9123a9592cc49aef53c57be4216823
jlefelhocz <jlefelhocz@yahoo.com>
2017-11-12 21:17:18 -0500

    More content, added inputs

--- scripts/stuff.js
+++ scripts/stuff.js
@@ -1,8 +1,9 @@
 function changeColor() {
     var x = document.getElementById("h5");
     x.style.color = "red";
+    x.innerHTML = "Welcome to Dystopia."
     var y = document.getElementById("p");
-    y.innerHTML = "Did you really think it would be that easy? Dystopias are the embodiment of everything humans don't desire. Good luck. You'll need it.";
+    y.innerHTML = "Did you really think it would be that easy? Utopia by definition, does not exist. Whenever people try to make Utopia, they end up with a dystopia. The following text game is based after Margaret Atwood's Novel, <i>Oryx and Crake</i>. The goal of this game is to survive Dystopia. Answer the questions as accurately as possible to your own beliefs and values, but remember your survival is dependent on your choices. Consider all possible options thoroughly before making a decision. Good luck. You'll need it.";
     var z = document.getElementById("button");
     var str = "Proceed";
     var result = str.link("Storypage.html");
